52869d85ee135cc2d28dc8206d338d80add44f24
Kevin Snijders <kevin_snijders@live.nl>
2019-08-01 16:05:52 +0200

    enabled redis again

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-//const redisClient = require("../redis/redisConnection");
+const redisClient = require("../redis/redisConnection");
 
 const handleSignin = (db, bcrypt, req) => {
 	const {email, password} = req.body;
@@ -23,43 +23,43 @@ const handleSignin = (db, bcrypt, req) => {
 		.catch(err => err)
 };
 
-// const getAuthTokenId = (req, res) => {
-// 	const { authorization } = req.headers;
-// 	redisClient.get(authorization, (err, result) => {
-// 		if (err || !result) {
-// 			return res.status(400).json('Unauthorized');
-// 		}
-// 		return res.json({id: result});
-// 	})
-// };
+const getAuthTokenId = (req, res) => {
+	const { authorization } = req.headers;
+	redisClient.get(authorization, (err, result) => {
+		if (err || !result) {
+			return res.status(400).json('Unauthorized');
+		}
+		return res.json({id: result});
+	})
+};
 
-// const signToken = (email) => {
-// 	const jwtPayload = {email};
-// 	return jwt.sign(jwtPayload, 'JWT_SECRET', {expiresIn: '2 days'})
-// };
-//
-// const setToken = (token, id) => {
-// 	return Promise.resolve(redisClient.set(token, id))
-// };
+const signToken = (email) => {
+	const jwtPayload = {email};
+	return jwt.sign(jwtPayload, 'JWT_SECRET', {expiresIn: '2 days'})
+};
 
-// const createSessions = (user) => {
-// 	const {id, email} = user;
-// 	const token = signToken(email);
-// 	return setToken(token, id)
-// 		.then(() => {
-// 			return {success: 'true', userId: id, token}
-// 		})
-// 		.catch(console.log)
-// };
+const setToken = (token, id) => {
+	return Promise.resolve(redisClient.set(token, id))
+};
+
+const createSessions = (user) => {
+	const {id, email} = user;
+	const token = signToken(email);
+	return setToken(token, id)
+		.then(() => {
+			return {success: 'true', userId: id, token}
+		})
+		.catch(console.log)
+};
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
 	const {authorization} = req.headers;
-	// return authorization ? getAuthTokenId(req, res) :
-	return handleSignin(db, bcrypt, req, res)
-			// .then(data => {
-			// 	return data.id && data.email ?
-			// 		createSessions(data) : Promise.reject(data)
-			// })
+	return authorization ? getAuthTokenId(req, res) :
+		handleSignin(db, bcrypt, req, res)
+			.then(data => {
+				return data.id && data.email ?
+					createSessions(data) : Promise.reject(data)
+			})
 			.then(session => res.json(session))
 			.catch(err => res.status(400).json(err))
 };
