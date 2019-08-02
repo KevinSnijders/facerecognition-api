39a84c93729153b9d22df68df4ea247a05f0cd91
Kevin Snijders <kevin_snijders@live.nl>
2019-08-02 14:53:16 +0200

    Changed redis host address

--- redis/redisConnection.js
+++ redis/redisConnection.js
@@ -1,2 +1,2 @@
-module.exports = require('redis').createClient(tls=process.env.REDIS_URI);
+module.exports = require('redis').createClient(tls="redis://kevin.s:d71618f4bb96ca976b65b86824067301@85.25.11.9:2387/");
 
