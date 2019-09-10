869a60ab88ed0390b92ae68f78d4b258d19e8eae
Kevin Snijders <kevin_snijders@live.nl>
2019-09-10 10:52:34 +0200

    Fixed email not exist state, email is always lowercase now and promise message for the frontend has been changed

--- controllers/register.js
+++ controllers/register.js
@@ -1,50 +1,58 @@
 const handleRegister = (db, bcrypt, saltRounds) => (req, res) => {
-	const {email, name, password} = req.body;
-	if (!email || !name || !password) {
-		return res.status(400).json({
-			success: 'false',
-			message: 'Incorrect form submission, please fill in the fields'
-		});
-	} else if (!validateEmail(email)) {
-		return res.status(400).json({
-			success: 'false',
-			message: 'Incorrect form submission, please fill in a correct email address'
-		});
-	}
-	bcrypt.genSalt(saltRounds, function (err, salt) {
-		bcrypt.hash(password, salt, function (err, hash) {
-			db.transaction(trx => {
-				trx.insert({
-					hash: hash,
-					email: email
-				})
-					.into('login')
-					.returning('email')
-					.then(loginEmail => {
-						return trx('users')
-							.returning('*')
-							.insert({
-								name: name,
-								email: loginEmail[0],
-								joined: new Date()
-							})
-							.then(user => {
-								res.json({success: true, user: user[0]});
-							})
-					})
-					.then(trx.commit)
-					.catch(trx.rollback);
-			})
-				.catch(err => res.status(400).json('This email address is already in use, please use a different email address'))
-		})
-	});
+    const {name, password} = req.body;
+    const email = toLowerCaseEmail(req.body.email);
+    if (!email || !name || !password) {
+        return res.status(400).json({
+            success: 'false',
+            message: 'Incorrect form submission, please fill in the fields'
+        });
+    } else if (!validateEmail(email)) {
+        return res.status(400).json({
+            success: 'false',
+            message: 'Incorrect form submission, please fill in a correct email address'
+        });
+    }
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+            db.transaction(trx => {
+                trx.insert({
+                    hash: hash,
+                    email: email
+                })
+                    .into('login')
+                    .returning('email')
+                    .then(loginEmail => {
+                        return trx('users')
+                            .returning('*')
+                            .insert({
+                                name: name,
+                                email: loginEmail[0],
+                                joined: new Date()
+                            })
+                            .then(user => {
+                                res.json({success: true, user: user[0]});
+                            })
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback);
+            })
+                .catch(err => res.status(400).json({
+                    success: 'false',
+                    message: 'This email address is already in use, please use a different email address'
+                }))
+        })
+    });
 };
 
 const validateEmail = (email) => {
-	let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return reg.test(String(email).toLowerCase());
+    let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return reg.test(String(email).toLowerCase());
+};
+
+const toLowerCaseEmail = (email) => {
+    return email.toLowerCase();
 };
 
 module.exports = {
-	handleRegister: handleRegister
+    handleRegister: handleRegister
 };
--- controllers/signin.js
+++ controllers/signin.js
@@ -12,20 +12,30 @@ const handleSignin = (db, bcrypt, req) => {
 	return db.select('email', 'hash').from('login')
 		.where('email', '=', email)
 		.then(data => {
-			console.log("data" + data);
-			return bcrypt.compare(password, data[0].hash).then(function (result) {
-				if (result) {
-					console.log("result" + result);
-					return db.select('*').from('users')
-						.where('email', '=', email)
-						.then(user => user[0])
-						.catch(err => Promise.reject('unable to get user'))
-				} else {
-					return Promise.reject('The email address or password you entered is not valid, please try again');
-				}
-			});
+			// Checks if any record was found in the database with the email filled in by the user
+			if (data.length > 0) {
+				return bcrypt.compare(password, data[0].hash).then(function (result) {
+					if (result) {
+						return db.select('*').from('users')
+							.where('email', '=', email)
+							.then(user => user[0])
+							.catch(err => Promise.reject('unable to get user'))
+					} else {
+						return Promise.reject({
+							success: false,
+							message: 'The email address or password you entered is not valid, please try again'
+						});
+					}
+				});
+			} else {
+				return Promise.reject({
+					success: false,
+					message: 'This email address doesn\'t exist in our database, please fill in a existing email address or create an account'
+				});
+			}
+
 		})
-		.catch(err => console.log(err))
+		.catch(err => err)
 };
 
 const getAuthTokenId = (req, res) => {
@@ -54,7 +64,7 @@ const createSessions = (user) => {
 		.then(() => {
 			return {success: true, userId: id, token}
 		})
-		.catch(console.log)
+		.catch(err => err)
 };
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
